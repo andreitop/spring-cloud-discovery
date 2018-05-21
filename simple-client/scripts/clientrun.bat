d55f5b6a14d9f8ed7ee84f3796beb7fe1b950915
Andrei Dremov <andrei_dremov@epam.com>
2018-05-22 02:42:02 +0300

    Add some fixes

--- simple-client/scripts/clientrun.bat
+++ simple-client/scripts/clientrun.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-start java -jar -DCLIENT_NO=1 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar
-start java -jar -DCLIENT_NO=2 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar
-start java -jar -DCLIENT_NO=3 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar
+start java -jar -DCLIENT_ID=1 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar
+start java -jar -DCLIENT_ID=2 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar
+start java -jar -DCLIENT_ID=3 -Xmx192m ../target/simple-client-1.0-SNAPSHOT.jar

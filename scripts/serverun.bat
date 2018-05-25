795bb340d8346f0e231903c1043a4505d202b36c
Andrei Dremov <andrei_dremov@epam.com>
2018-05-25 16:43:24 +0300

    Implement gateway server zuul

--- scripts/serverun.bat
+++ scripts/serverun.bat
@@ -2,3 +2,4 @@
 start java -jar -Dspring.profiles.active=peer1 -Xmx192m ../simple-server/target/simple-server-1.0-SNAPSHOT.jar
 start java -jar -Dspring.profiles.active=peer2 -Xmx192m ../simple-server/target/simple-server-1.0-SNAPSHOT.jar
 start java -jar -Dspring.profiles.active=peer3 -Xmx192m ../simple-server/target/simple-server-1.0-SNAPSHOT.jar
+start java -jar -Xmx192m ../simple-gateway/target/simple-gateway-1.0-SNAPSHOT.jar
